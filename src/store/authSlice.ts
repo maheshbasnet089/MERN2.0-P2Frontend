7413915d287dd5ddfe5b2d5f5bace5be8957e2a7
maheshbasnet089 <basnetmanish089@gmail.com>
2024-05-06 21:07:21 +0545

    first commit

---
+++ src/store/authSlice.ts
@@ -0,0 +1,34 @@
+import {createSlice,PayloadAction} from '@reduxjs/toolkit' 
+
+interface User{
+    username : string, 
+    email : string, 
+    password : string, 
+    token : string
+}
+
+interface AuthState{
+    user : User,
+    status : string
+}
+
+const initialState:AuthState = {
+    user : {} as User,
+    status : "loading"
+}
+
+const authSlice = createSlice({
+    name : 'auth',
+    initialState,
+    reducers : {
+        setUser(state:AuthState,action:PayloadAction<User>){
+            state.user = action.payload
+        },
+        setStatus(state:AuthState,action:PayloadAction<string>){
+            state.status = action.payload
+        }
+    }
+})
+
+ export const {setUser,setStatus} = authSlice.actions 
+ export default authSlice.reducer
---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit'
+import authSlice from './authSlice'
+import blogSlice from './blogSlice'
+
+const store = configureStore({
+    reducer : {
+       auth : authSlice ,
+       blog : blogSlice
+    }
+})
+
+export default store 
