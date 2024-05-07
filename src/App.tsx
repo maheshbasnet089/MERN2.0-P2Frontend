f8c01650d924ab91393eb2f2300c0acbdbe54e81
maheshbasnet089 <basnetmanish089@gmail.com>
2024-05-07 20:20:10 +0545

    connect react with redux t

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 
 
-import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import store from './store/store'
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <Provider store={store}>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<h1>This is home Page</h1>} />
+      <Route path='/register' element={<h1>This is register page</h1>} />
+      <Route path='/login' element={<h1>This is login page</h1>} />
+    </Routes>
+  </BrowserRouter>
+  </Provider>
+  
+
   )
 }
 
